655044b34dae135316a693f1a23f03c1ebc6268a
mdnurakmal <mdnurakmal@gmail.com>
2021-09-16 17:46:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from distributed_counters import Shard
+from distributed_counters import increment
 
 app = Flask(__name__)
 
 @app.route('/increment/', methods=['GET', 'POST'])
 def welcome():
     print("hello")
+    increment()
     return "Hello World!"
-
 
 
 if __name__ == "__main__":
