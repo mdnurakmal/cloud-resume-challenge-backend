e5a31c3779554e3978710c063b287729ecd7386e
mdnurakmal <mdnurakmal@gmail.com>
2021-09-18 00:46:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def welcome():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ['PORT']))
