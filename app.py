1068aa5b73e438abff525cfca3bb5c6b4d843a2e
mdnurakmal <mdnurakmal@gmail.com>
2021-09-18 13:08:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def welcome():
     }
 
    
-    return value
+    return json.dumps(value)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
