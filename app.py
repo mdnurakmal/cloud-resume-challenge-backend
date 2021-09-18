09641373d9a87800e1cd55bec23084d23f63f6e5
mdnurakmal <mdnurakmal@gmail.com>
2021-09-18 11:58:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,26 +8,13 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['GET'])
-def welcome(request):
-    # For more information about CORS and CORS preflight requests, see:
-    # https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+def welcome():
 
-    # Set CORS headers for the preflight request
-    if request.method == 'OPTIONS':
-        # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
-        headers = {
+    headers = {
             'Access-Control-Allow-Origin': 'https://oneclicklaunch.com',
             'Access-Control-Allow-Methods': 'GET',
             'Access-Control-Allow-Headers': 'Content-Type',
             'Access-Control-Max-Age': '3600'
-        }
-
-        return ('', 204, headers)
-
-    # Set CORS headers for the main request
-    headers = {
-        'Access-Control-Allow-Origin': '*'
     }
 
     return ('Hello World!', 200, headers)
