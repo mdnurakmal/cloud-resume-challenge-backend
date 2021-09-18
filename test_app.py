5287768b231a8935061ca2964d309594acb33741
mdnurakmal <mdnurakmal@gmail.com>
2021-09-19 02:16:51 +0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,4 +7,4 @@ def test_hello():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
-    assert response.data.count == b'count'
+    assert len(response.json()['count']) == 1
