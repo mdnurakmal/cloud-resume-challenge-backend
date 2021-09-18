b5b743402a7c9e7f93e7fd20f2b30d8cd5be76d1
mdnurakmal <mdnurakmal@gmail.com>
2021-09-18 11:38:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def welcome(request):
         # Allows GET requests from any origin with the Content-Type
         # header and caches preflight response for an 3600s
         headers = {
-            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Origin': 'https://oneclicklaunch.com',
             'Access-Control-Allow-Methods': 'GET',
             'Access-Control-Allow-Headers': 'Content-Type',
             'Access-Control-Max-Age': '3600'
