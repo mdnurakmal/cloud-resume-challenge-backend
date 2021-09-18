0999db791d0ffd53e2b07166f94336a3a701be59
mdnurakmal <mdnurakmal@gmail.com>
2021-09-18 22:34:47 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,7 @@ locals {
 
 # Create a service account
 resource "google_service_account" "cloud_resume_challenge_worker" {
+  project = var.project
   account_id   = "crc-worker-id"
   display_name = "Cloud Resume Challenge Worker"
 }
