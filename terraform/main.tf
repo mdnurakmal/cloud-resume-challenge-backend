883aea2fb641c93ba31cb1298006e96f85777272
mdnurakmal <mdnurakmal@gmail.com>
2021-09-20 22:22:05 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,17 +6,22 @@ terraform {
     }
   }
 
-    backend "local" {
+    backend "gcs" {
+    bucket  = "cloud-resume-challenge-bucket"
+    prefix  = "terraform/state"
   }
 
 }
 
-provider "google" {
+provider "google" {}
 
+data "google_project" "project" {
 }
 
 
 locals {
+  project = data.google_project.project.project_id
+  project_number = data.google_project.project.number
   service_name   = "cloud-resume-challenge"
   cloud_resume_challenge_worker_sa  = "serviceAccount:${google_service_account.cloud_resume_challenge_worker.email}"
 }
